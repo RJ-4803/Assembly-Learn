3a23ce65ccc8ca651f5a897023aff1c4b1e05cad
RJ-4803 <renujaan4803@gmail.com>
2026-01-08 14:19:47 +0530

    feat: add new file

---
+++ AddingInegers.asm
@@ -0,0 +1,12 @@
+.data
+    num1: .word 4
+    num2: .word 8
+.text
+    lw $t0, num1($zero)
+    lw $t1, num2($zero)
+    
+    add $t2, $t0, $t1
+    
+    li $v0,1
+    add $a0, $zero, $t2
+    syscall
